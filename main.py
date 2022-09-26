929c7b68d86ed7829a41d4d1e0776d617d66ea8a
adityasudini <sudini@usc.edu>
2022-09-26 07:15:29 -0700

    week4

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import pandas as pd
 
 app = create_app()
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port='8090', host='localhost')
+    app.run(debug=True, port='8080', host='localhost')
 
 
