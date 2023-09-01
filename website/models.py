f3184a642dcfb5a6c9b209d22af0c7c4069e65a3
adityasudini <sudini@usc.edu>
2023-09-01 13:15:21 -0700

    09012023

--- website/models.py
+++ website/models.py
@@ -9,7 +9,6 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=False)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
 
 
 
