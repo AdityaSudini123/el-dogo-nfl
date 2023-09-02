5aa31c3e49d7f956b7dce5eab8e813927d595b00
adityasudini <sudini@usc.edu>
2023-09-02 11:53:01 -0700

    2023 commit 2

--- website/__init__.py
+++ website/__init__.py
@@ -10,12 +10,13 @@ def create_app():
     app = Flask(__name__)
     with app.app_context():
         app.config['SECRET_KEY'] = "helloworld"
+        app.config['SESSION_PERMANENT'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.init_app(app)
 
         from .views import views
-        from .auth2 import auth
+        from .auth import auth
 
         app.register_blueprint(views, url_prefix="/")
         app.register_blueprint(auth, url_prefix="/")
